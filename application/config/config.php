e45993752cb6d0c535411b6557871c745b084cdc
Yue Lyu <lyu.yue1992@gmail.com>
2015-10-25 20:57:08 -0400

    fix base URL

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'dbw';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
