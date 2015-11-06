b6b9b8e7e5efff1fc9391d7249f0a357a5241d93
Yue Lyu <lyu.yue1992@gmail.com>
2015-11-05 21:38:44 -0500

    home added

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YueLyu
+ * Date: 11/5/15
+ * Time: 9:32 PM
+ */
+
+class Home extends CI_Controller {
+
+    public function index()
+    {
+        $echo "lalala";
+    }
+
+}
